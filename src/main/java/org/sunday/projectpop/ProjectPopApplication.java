ab592a6214d4aabf67c11d40128ea257b397c23f
gitnewbb <tal6314@gmail.com>
2025-04-29 17:06:34 +0900

    first commit

---
+++ src/main/java/org/sunday/projectpop/ProjectPopApplication.java
@@ -0,0 +1,13 @@
+package org.sunday.projectpop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPopApplication.class, args);
+    }
+
+}
