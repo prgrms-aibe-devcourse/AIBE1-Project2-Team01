1854eaff6477803dde2fac4873df7b323ca105e6
Juyoung8563 <zxcvbnm856311@gmail.com>
2025-05-12 09:32:18 +0900

    feat: Spring Security & JWT & 소셜 로그인 기능 및 설정 로직 구현

---
+++ src/main/java/org/sunday/projectpop/newnew/service/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package org.sunday.projectpop.newnew.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.sunday.projectpop.newnew.entity.Users;
+import org.sunday.projectpop.newnew.repository.UsersRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsersRepository usersRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Users user = usersRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + email));
+
+        return User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
+}
